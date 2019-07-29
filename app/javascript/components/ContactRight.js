6d6fd9ba996bcd7bf4f507cc4fe36148d7a74842
Remon <remmony@gmail.com>
2019-07-29 18:26:41 +0200

    update webpacker.yml

--- app/javascript/components/ContactRight.js
+++ app/javascript/components/ContactRight.js
@@ -1,16 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import envColor1Img from 'images/env_color1.png';
 class ContactRight extends React.Component {
   render() {
     return (
-      <div className="col-md-6 col-sm-12">
-        <div className="right_contact">
-          <div className="centery">
-            <img src="/assets/env_color1.png" className="env_img" />
+      <div className='col-md-6 col-sm-12'>
+        <div className='right_contact'>
+          <div className='centery'>
+            <img src={envColor1Img} className='env_img' />
           </div>
 
           <div>
-            <ul className="ul_contact">
+            <ul className='ul_contact'>
               <li> You can send me an email to info@remon.info </li>
 
               <li> Whatsapp +201063206700 </li>
