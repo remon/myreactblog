597030582fcb8465abd65cf53d3a3592ba22e294
Remon <remmony@gmail.com>
2019-07-30 02:37:26 +0200

    remove compile_assets_locally task from capistrano tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,42 +27,10 @@ set :linked_files, %w{config/database.yml config/master.key}
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, %w{ log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system }
-# Default value for :format is :pretty
-# set :format, :pretty
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
 set :tests, []
-#set :pty, true
-# namespace :deploy do
-#   desc "Restart application"
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
 
-#   after :publishing, :restart
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-# end
 set(:config_files, %w(
   nginx.conf
   database.example.yml
@@ -101,31 +69,8 @@ set(:symlinks, [
 # http://www.capistranorb.com/documentation/getting-started/flow/
 # is worth reading for a quick overview of what tasks are called
 # and when for `cap stage deploy`
-# namespace :yarn do
-#   desc "Install yarn dependencies"
-#   task :install do
-#     on roles(:app) do
-#       within release_path do
-#         execute :yarn, :install
-#       end
-#     end
-#   end
 
-#   desc "yarn dependencies"
-#   task :build do
-#     on roles(:app) do
-#       within release_path do
-#         with node_env: :production do
-#           execute :yarn, :build
-#         end
-#       end
-#     end
-#   end
-# end
 namespace :deploy do
-  # before :updated, "yarn:install"
-  # before :updated, "yarn:build"
-  #before "deploy:assets:precompile", "deploy:yarn_install"
 
   # make sure we're deploying what we think we're deploying
   before :deploy, "deploy:check_revision"
