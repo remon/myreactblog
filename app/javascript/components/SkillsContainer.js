522a9048f2b0b3f7094ad097f29a22806a268565
Remon <remmony@gmail.com>
2019-07-30 02:45:16 +0200

    change docker skills :bulb:

--- app/javascript/components/SkillsContainer.js
+++ app/javascript/components/SkillsContainer.js
@@ -1,27 +1,27 @@
-import React from "react";
-import SkillsWrapper from "../components/SkillsWrapper";
+import React from 'react';
+import SkillsWrapper from '../components/SkillsWrapper';
 
 class SkillsContainer extends React.Component {
   render() {
     const mySkills1 = [
-      { level: 80, name: "Ruby" },
-      { level: 90, name: "JavaScript" },
-      { level: 90, name: "jQuery/AJAX" },
-      { level: 60, name: "Python" },
-      { level: 70, name: "Docker" },
-      { level: 80, name: "Redis" },
-      { level: 70, name: "CI/CD" },
-      { level: 80, name: "HTML/CSS" }
+      { level: 80, name: 'Ruby' },
+      { level: 90, name: 'JavaScript' },
+      { level: 90, name: 'jQuery/AJAX' },
+      { level: 60, name: 'Python' },
+      { level: 40, name: 'Docker' },
+      { level: 80, name: 'Redis' },
+      { level: 70, name: 'CI/CD' },
+      { level: 80, name: 'HTML/CSS' }
     ];
     const mySkills2 = [
-      { level: 90, name: "Ruby On Rails" },
-      { level: 80, name: "NodeJs" },
-      { level: 80, name: "React" },
-      { level: 80, name: "Bootstrap" },
-      { level: 80, name: "TDD/Rspec" },
-      { level: 80, name: "MongoDB" },
-      { level: 90, name: "Git" },
-      { level: 80, name: "Heroku" }
+      { level: 90, name: 'Ruby On Rails' },
+      { level: 80, name: 'NodeJs' },
+      { level: 80, name: 'React' },
+      { level: 80, name: 'Bootstrap' },
+      { level: 80, name: 'TDD/Rspec' },
+      { level: 80, name: 'MongoDB' },
+      { level: 90, name: 'Git' },
+      { level: 80, name: 'Heroku' }
     ];
     const leftSkills = mySkills1.map((item, index) => {
       return <SkillsWrapper name={item.name} level={item.level} key={index} />;
@@ -30,14 +30,14 @@ class SkillsContainer extends React.Component {
       return <SkillsWrapper name={item.name} level={item.level} key={index} />;
     });
     return (
-      <div className=" skills-container" id="skills">
-        <div className="wmiya inline_b">
-          <h2 className="hig"> My Skills </h2>
+      <div className=' skills-container' id='skills'>
+        <div className='wmiya inline_b'>
+          <h2 className='hig'> My Skills </h2>
         </div>
-        <div className="col skills-main">
-          <div className="row">
-            <div className="col-md-6 col-sm-12">{leftSkills}</div>
-            <div className="col-md-6 col-sm-12">{rightSkills}</div>
+        <div className='col skills-main'>
+          <div className='row'>
+            <div className='col-md-6 col-sm-12'>{leftSkills}</div>
+            <div className='col-md-6 col-sm-12'>{rightSkills}</div>
           </div>
         </div>
       </div>
